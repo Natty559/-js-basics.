c460773ffdc6d52d5eb876ed42b9fc6c50dc68a5
Natty559 <nathanielanababawa@gmail.com>
2026-03-06 20:46:58 +0000

    Added JavaScript assignments

---
+++ assignment1.js
@@ -0,0 +1,18 @@
+// Assignment 1 - The Truth Table (Comparison Operators)
+
+// Declare variables
+let requiredAge = 18;
+let userAge = "18"; // string
+
+// Loose equality (compares value only)
+console.log(userAge == requiredAge);
+
+// Strict equality (compares value AND type)
+console.log(userAge === requiredAge);
+
+// Not strictly equal
+console.log(userAge !== requiredAge);
+
+// Bonus
+console.log(5 > 2);
+console.log(10 <= 10);
